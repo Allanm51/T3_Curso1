a3a868a800c7f72b31195c805206c940952643f1
Allanm51 <allan.santos@bsatech.io>
2025-10-15 17:25:12 -0300

    unidade 4

---
+++ Alura.Adopet.Testes/HttpClientPetTest.cs
@@ -0,0 +1,32 @@
+﻿using Alura.Adopet.Console.Servicos;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Testes
+{
+    public class HttpClientPetTest
+    {
+        [Fact]
+        public async Task ListaPetsDeveRetornarUmaListaVazia()
+        {
+            //Arange
+            var clientePet = new HttpClientPet();
+
+            //Act
+            var lista = await clientePet.ListPetsAsync();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.NotEmpty(lista);
+        }
+
+        [Fact]
+        public async Task QuandoAPIForaDeveRetornarUmaExcecao()
+        {
+            //Arrange
+            var clientePet = new HttpClientPet(uri:"http://localhost:5000");
+
+            //Act+Assert
+            await Assert.ThrowsAnyAsync<Exception>(()=> clientePet.ListPetsAsync());
+        }
+    }
+}
